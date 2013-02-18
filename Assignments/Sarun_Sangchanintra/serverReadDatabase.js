8ae2ca42b86f5b5f21cb0e7661645e7b7dabe511
golfsarun <golffy_aoi@yahoo.com>
2013-02-18 05:09:33 -0800

    Create serverReadDatabase.js

---
+++ Assignments/Sarun_Sangchanintra/serverReadDatabase.js
@@ -0,0 +1,33 @@
+var http = require('http');
+
+var parseXlsx = require('excel');
+
+
+http.createServer( function(req, res) {
+
+  console.log('Server Started ');
+
+  parseXlsx('Currency.xlsx', function(data) {
+
+      res.writeHead(200, {'content-type': 'text/plain'});
+
+      res.write('Welcome to the Spreadsheet reader !\n');
+
+      var row   = data[0];
+      var cell = row[0];
+
+      res.write("Database cell = ");
+      res.write(cell);
+      res.write("\n");
+
+      res.end("Good Bye !\n");
+
+      console.log("Database cell = ", cell);
+
+  });
+
+
+}).listen(8121);
+
+
+console.log('Server running on port 8121');
